a6f07d3bcc4819ff45cfbce6c5ff4bd8aeac9f98
AbhishekOjha <abhishekojha38@gmail.com>
2026-07-09 23:34:16 -0400

    nvim: add more plugins

--- .config/nvim/lua/abhio/lazy.lua
+++ .config/nvim/lua/abhio/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("abhio.plugins")
+require("lazy").setup("abhio.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
